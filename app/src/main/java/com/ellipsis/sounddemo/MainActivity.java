4c151b78a49ca59ba266ebdc031bba0de97cac7c
Brunthavan <vimosanan.13.cse@gmail.com>
2018-07-23 18:48:32 +0530

    seekbar annexed

--- app/src/main/java/com/ellipsis/sounddemo/MainActivity.java
+++ app/src/main/java/com/ellipsis/sounddemo/MainActivity.java
@@ -1,13 +1,23 @@
 package com.ellipsis.sounddemo;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
+    TextView volumeLevel;
 
     public void playPressed(View view){
         Button btnPlay = (Button) findViewById(R.id.btnPlay);
@@ -30,6 +40,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mediaPlayer = MediaPlayer.create(this, R.raw.chinmayee);
-//        btnPause.setClickable(false);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        volumeLevel = (TextView) findViewById(R.id.txtVolume);
+
+        SeekBar volumeController = (SeekBar) findViewById(R.id.volumeSeekBar);
+        final SeekBar scrubSeekBar = (SeekBar) findViewById(R.id.scrubSeekBar);
+
+        final int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        volumeLevel.setText(Integer.toString(100* currentVolume/maxVolume));
+
+        volumeController.setMax(maxVolume);
+        volumeController.setProgress(currentVolume  );
+
+        volumeController.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+                volumeLevel.setText(Integer.toString(100*i/maxVolume));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        scrubSeekBar.setMax(mediaPlayer.getDuration());
+        scrubSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("Seekbar Changed", Integer.toString(i));
+                mediaPlayer.seekTo(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                scrubSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0, 1000);
+
+
+
+
     }
 }
